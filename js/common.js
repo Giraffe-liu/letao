8756a6ae22c2682ab6c1296450205b5121a0b8b2
“g“Giraffe-liu <18202530619@163.com>
2018-10-11 15:47:58 +0800

    后台登录模块完成

--- js/common.js
+++ js/common.js
@@ -6,3 +6,14 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	//添加登录拦截,验证是否登录
+	$.ajax({
+		url:'/api/employee/checkRootLogin',
+		success:function(msg){
+			console.log(msg);
+			if(msg.error){
+				location.href = '/login.html?url='+location.href;
+			}
+		}
+	})
